e7595c67afacd2d4e977c1acacaf6fcc443d4939
Sagar <worthless.dumboo@gmail.com>
2022-04-21 22:56:23 -0500

    feat: AuthenticationContext used with protected routes

--- lab7/src/pages/PageRoutes/PageRoutes.jsx
+++ lab7/src/pages/PageRoutes/PageRoutes.jsx
@@ -1,18 +1,30 @@
+import { useContext, useState } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import NewPost from "../../components/newPost/NewPost";
 import PostDetails from "../../components/postDetails/postDetails";
 import PostList from "../../containers/PostList/PostList";
+import { AuthContext } from "../../contexts/AuthContext";
 import Login from "../login/Login";
 
 const PageRoutes = () => {
+  const { authUser } = useContext(AuthContext);
+
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/posts" />} />
-      <Route path="posts" element={<PostList />}>
-        <Route path=":id" element={<PostDetails />} />
-      </Route>
-      <Route path="create-post" element={<NewPost />} />
-      <Route path="login" element={<Login />} />
+      {authUser && (
+        <>
+          <Route path="posts" element={<PostList />}>
+            <Route path=":id" element={<PostDetails />} />
+          </Route>
+          <Route path="create-post" element={<NewPost />} />
+        </>
+      )}
+      {!authUser && <Route path="login" element={<Login />} />}
+
+      <Route
+        path="*"
+        element={<Navigate to={authUser ? "/posts" : "/login"} />}
+      />
     </Routes>
   );
 };
--- lab7/src/pages/dashboard/Dashboard.jsx
+++ lab7/src/pages/dashboard/Dashboard.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import Header from "../../containers/Header/Header";
+import { AuthContext } from "../../contexts/AuthContext";
 import PageRoutes from "../PageRoutes/PageRoutes";
 
 const Dashboard = () => {
+  const [authUser, setAuthUser] = useState(null);
+
   return (
     <React.Fragment>
-      <div className="header">
-        <Header />
-      </div>
-      <div className="post">
-        <PageRoutes />
-      </div>
+      <AuthContext.Provider value={{ authUser, setAuthUser }}>
+        <div className="header">
+          <Header />
+        </div>
+        <div className="post">
+          <PageRoutes />
+        </div>
+      </AuthContext.Provider>
     </React.Fragment>
   );
 };
